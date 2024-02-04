e4402e39ac9af4d47fc1265dd14cc122aa9e4483
jhonduarte7 <johnduarte7@gmail.com>
2024-02-03 21:59:42 -0300

    Login

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities/")
 const errorRoute = require("./routes/error.js")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute.js")
+const bodyParser = require("body-parser")
 
 
 /* ***********************
@@ -22,6 +26,32 @@ const errorRoute = require("./routes/error.js")
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * Routes
@@ -35,6 +65,9 @@ app.use("/inv", inventoryRoute)
 
 // Intentional 500 error
 app.use("/error", errorRoute)
+
+// Account route
+app.use("/account", accountRoute)
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
